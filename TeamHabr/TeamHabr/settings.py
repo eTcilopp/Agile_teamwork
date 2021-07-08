d2092d7221a36fe0240cbb70048dcd47aab80050
eTcilopp <kirikeza@gmail.com>
2021-07-07 20:46:21 -0600

    Меняю настройки почты

--- TeamHabr/TeamHabr/settings.py
+++ TeamHabr/TeamHabr/settings.py
@@ -193,11 +193,16 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 MAILER_EMAIL_BACKEND = EMAIL_BACKEND
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
 EMAIL_HOST_USER = 'teamhabr@gmail.com'
 # EMAIL_HOST_PASSWORD = '*.6fm^"GhqxW\_P('
 EMAIL_HOST_PASSWORD = 'kkwhdxpasqzvkkle'
+
+
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USE_TLS = False
 
 # App base password kkwhdxpasqzvkkle
 
